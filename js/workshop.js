b470c1eae752d7d63d020693a209f99471b8cc88
Muthia Rihadatul Aisyi <muthiarihadatul@gmail.com>
2025-10-20 21:32:24 +0700

    commmit ke 35

--- js/workshop.js
+++ js/workshop.js
@@ -96,189 +96,189 @@ const certificates = [
     name: "Muh. Amirul Ramadhanu",
     id: "DASTRAN-WS-10-2526-001",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/1.pdf"
   },
 
   {
     name: "Kevin Imanuel Saputra Purba",
     id: "DASTRAN-WS-10-2526-002",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/2.pdf"
   },
 
   {
     name: "Fazza Yuan Ikhwannaya",
     id: "DASTRAN-WS-10-2526-003",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/3.pdf"
   },
 
   {
     name: "Kharisma Akbar",
     id: "DASTRAN-WS-10-2526-004",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/4.pdf"
   },
 
   {
     name: "Samuel Chakra Vijaya Lumban Gaol",
     id: "DASTRAN-WS-10-2526-005",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/5.pdf"
   },
 
   {
     name: "Keisya Fajriani",
     id: "DASTRAN-WS-10-2526-006",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/6.pdf"
   },
 
   {
     name: "Fiqri Achmad Ardendi",
     id: "DASTRAN-WS-10-2526-007",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/7.pdf"
   },
 
   {
     name: "Kevin Raihan Adianto",
     id: "DASTRAN-WS-10-2526-008",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/8.pdf"
   },
 
   {
     name: "Syifa Agnesia ",
     id: "DASTRAN-WS-10-2526-009",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/9.pdf"
   },
 
   {
     name: "Gabriel Nathan Noel Angelo",
     id: "DASTRAN-WS-10-2526-010",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/10.pdf"
   },
 
   {
     name: "Salman Al Farisi",
     id: "DASTRAN-WS-10-2526-011",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/11.pdf"
   },
 
   {
     name: "Muhamad Agisya Cahya Ramadhan ",
     id: "DASTRAN-WS-10-2526-012",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/12.pdf"
   },
 
   {
     name: "Ana Tasya Putri",
     id: "DASTRAN-WS-10-2526-013",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/13.pdf"
   },
 
   {
     name: "Yales Nof Romy T. Mendrofa",
     id: "DASTRAN-WS-10-2526-014",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/14.pdf"
   },
 
   {
     name: "Falisha Salsabila",
     id: "DASTRAN-WS-10-2526-015",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/15.pdf"
   },
 
   {
     name: "Nabila Sintya Wardhani",
     id: "DASTRAN-WS-10-2526-016",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/16.pdf"
   },
 
   {
     name: "Fabio Syah Alvin",
     id: "DASTRAN-WS-10-2526-017",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/17.pdf"
   },
 
   {
     name: "Muhammad Fadhli Afif Al Azizi",
     id: "DASTRAN-WS-10-2526-018",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/18.pdf"
   },
 
   {
     name: " Kayla Nanda Nihan A.P",
     id: "DASTRAN-WS-10-2526-019",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/19.pdf"
   },
 
   {
     name: "Ghaisan Rabbani ",
     id: "DASTRAN-WS-10-2526-020",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/20.pdf"
   },
 
   {
     name: "Aldefan Yazdan Attaqee",
     id: "DASTRAN-WS-10-2526-021",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/21.pdf"
   },
 
   {
     name: "Lutfi Riyani Pratiwi",
     id: "DASTRAN-WS-10-2526-022",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/22.pdf"
   },
 
   {
     name: "Amirul Haqi Mardistya",
     id: "DASTRAN-WS-10-2526-023",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/23.pdf"
   },
 
   {
     name: "Mario Stiven Naibaho",
     id: "DASTRAN-WS-10-2526-024",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/24.pdf"
   },
 
   {
     name: "Daffa Maulana Firdaus",
     id: "DASTRAN-WS-10-2526-025",
     workshop: "Workshop Dastran 2025",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/25.pdf"
   },
 
   {
     name: "Amirul Haqi Mardistya",
     id: "DASTRAN-WS-10-2526-026",
     workshop: "Workshop Dastran 2025 - Peserta Terbaik",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/26.pdf"
   },
 
   {
     name: "Ghaisan Rabbani ",
     id: "DASTRAN-WS-10-2526-027",
     workshop: "Workshop Dastran 2025 - Peserta Terbaik",
-    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/sertif.pdf"
+    file: "https://dastranlaboratory.com/asset/sertifikat_workshop/27.pdf"
   }
 
 ];
